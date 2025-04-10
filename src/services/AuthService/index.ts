739d04b3b75c68ed9a0ec471715a387710d2099d
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-11 00:23:37 +0600

    implement user registration API call using react-hook-form data

---
+++ src/services/AuthService/index.ts
@@ -0,0 +1,12 @@
+import { FieldValues } from "react-hook-form";
+
+export const registerUser = async (userData: FieldValues) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/user`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+  return res.json();
+};
