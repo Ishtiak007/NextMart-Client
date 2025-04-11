b10a9de74921fc1268dfdbd7c0715625ade4d197
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-11 15:06:27 +0600

    Setting and Retrieving Logged-In User Info from Cookies

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
+import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
 
 // registerUser
 export const registerUser = async (userData: FieldValues) => {
@@ -28,8 +30,25 @@ export const loginUser = async (userData: FieldValues) => {
       },
       body: JSON.stringify(userData),
     });
-    return res.json();
+
+    const result = await res.json();
+    if (result.success) {
+      (await cookies()).set("accessToken", result.data.accessToken);
+    }
+
+    return result;
   } catch (error: any) {
     return Error(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = (await cookies()).get("accessToken")!.value;
+  let decodedData = null;
+  if (accessToken) {
+    decodedData = await jwtDecode(accessToken);
+    return decodedData;
+  } else {
+    return null;
   }
 };
