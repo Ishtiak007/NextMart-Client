3202ddb4c695434083a9d51bc9f7e2404ff61a03
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-11 15:33:48 +0600

    create UserProvider context for managing current user state

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,38 @@
+import { getCurrentUser } from "@/services/AuthService";
+import { IUser } from "@/types";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+
+interface IUserProviderValues {
+  user: IUser | null;
+  isLoading: boolean;
+  setUser: (user: IUser | null) => void;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+const userContext = createContext<IUserProviderValues | undefined>(undefined);
+
+const UserProvider = ({ childern }: { childern: React.ReactNode }) => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const handleUser = async () => {
+    const user = await getCurrentUser();
+    setUser(user);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    handleUser();
+  }, [isLoading]);
+  return (
+    <userContext.Provider value={{ user, setUser, isLoading, setIsLoading }}>
+      {childern}
+    </userContext.Provider>
+  );
+};
+
+export default UserProvider;
