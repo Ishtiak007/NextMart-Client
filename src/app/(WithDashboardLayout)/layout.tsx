f7438e2bb1e13dc2de0e2ed1876b19532c32d137
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-04-12 21:42:23 +0600

    Modified Sidebar

--- src/app/(WithDashboardLayout)/layout.tsx
+++ src/app/(WithDashboardLayout)/layout.tsx
@@ -44,11 +44,13 @@ export default function DashboardLayout({
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-white" />
+            <div className="aspect-video rounded-xl bg-white" />
+            <div className="aspect-video rounded-xl bg-white" />
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-white md:min-h-min">
+            {children}
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
