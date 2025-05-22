4ae7cada1fbdbc6c3b29c0422f45fb673f23bd3d
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-05-22 14:23:27 +0600

    Modify Data & Add Product to Shop

---
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Slide from "@/components/modules/HomePage/Banner";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Slide />
+    </div>
+  );
+};
+
+export default HomePage;
